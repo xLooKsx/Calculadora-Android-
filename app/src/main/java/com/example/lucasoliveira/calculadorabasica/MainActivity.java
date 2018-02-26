f63dc68a298707c7848079c07e1c2c8d5e8abd14
Lucas Oliveira <lu.oliveira.silva98@gmail.com>
2018-02-26 16:06:05 -0300

    Layout finalizado

---
+++ app/src/main/java/com/example/lucasoliveira/calculadorabasica/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.lucasoliveira.calculadorabasica;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.*; //usada para manipular componentes
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText vlr1;
+    private EditText vlr2;
+    private EditText vlrresult;
+
+    private Button somar;
+    private Button dividir;
+    private Button multiplicar;
+    private Button subtrair;
+
+    private double num1;
+    private double num2;
+    private double result;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
